f96f091b87a3256fc7a4f9c83d76d351e8c60c26
Sujan Chegu <sujanchg@gmail.com>
2021-04-12 13:36:10 +0530

    Add regUser

--- code/dbase.py
+++ code/dbase.py
@@ -3,44 +3,56 @@ from sqlite3 import Error
 
 class DBase():
     def __init__(self):
-        __conn = None
+        self.__conn = None
         try:
-            __conn = sqlite3.connect('../assets/database.db')
-            __conn.execute('DROP TABLE videos')
-            __conn.execute('CREATE TABLE videos (name TEXT, uri TEXT,\
+            self.__conn = sqlite3.connect('../assets/database.db')
+            
+            self.__conn.execute('DROP TABLE videos')
+            self.__conn.execute('CREATE TABLE videos (name TEXT, uri TEXT,\
             desc TEXT, likes INTEGER, views INTEGER, path BLOB, \
             uid TEXT, dur REAL)')
+
+            self.__conn.execute('DROP TABLE users')
+            self.__conn.execute('CREATE TABLE users (name TEXT, email TEXT,\
+            uid TEXT, passwd TEXT, regDate TEXT, creditCard TEXT,\
+            isCreator INTEGER)')
+
         except Error as e:
             print(e)
 
-    def saveToDB(self):
-        pass
-
-    def deleteFromDB(self, vid_uri):
+    def deleteVideoFromDB(self, iURI):
         try:
-            __conn.execute('DELETE FROM {table} WHERE {condition}',
-            table = 'videos', condition = 'uri = ' + vid_uri)
+            self.__conn.execute('DELETE FROM videos WHERE {condition}',
+            condition = 'uri = ' + iURI)
         except Error as e:
             print(e)
 
-    def calcSimilarity(self, vid_uri1, vid_uri2):
+    def calcSimilarity(self, iURI1, iURI2):
         pass
     
-    def incAttr(self, vid_uri, attr, count=1):
+    def incAttr(self, iURI, attr, count=1):
         try:
-            self.__conn.execute(f'UPDATE {table} SET {bute} = {bute} \
-            + {cnt} WHERE {condition}', table='videos', bute=attr,
-            cnt = count, condition='uri = ' + vid_uri)
+            self.__conn.execute(f'UPDATE videos SET {bute} = {bute} \
+            + {cnt} WHERE {condition}', bute=attr,
+            cnt = count, condition='uri = ' + iURI)
         except Error as e:
             print(e)
 
-    # def retreive_videopath(self, vid_uri):
-    #     try:
-    #         cur = self.__conn.cursor()
-    #         path = cur.execute("SELECT path FROM {_table} WHERE \
-    #         {condition}", _table = 'videos', condition = 'uri = ' + vid_uri)
-    #         return path
-    #     except Error as e:
-    #         print(e)
+    def regUser(self, iName, iEmail, iUID, iPassword, iRegDate, iCCard, iIsCreator):
+        try:
+            self.__conn.execute('INSERT INTO users VALUES {name}, {email},\
+            {uid}, {passwd}, {regDate}, {creditCard}, {isCreator}',
+            name=iName, email=iEmail, uid=iUID, passwd=iPassword,
+            regDate=iRegDate, creditCard=iCCard, isCreator=iIsCreator)
+        except Error as e:
+            print(e)
+
+    def regCreator(iUID):
+        try:
+            self.__conn.execute('UPDATE users SET isCreator = 1 WHERE {condition}',
+            condition = 'uid = '+iUID)
+        except Error as e:
+            print(e)
+
 
 db = DBase()
