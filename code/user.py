cb55631a3372197e0ffb467c275e65c854b17557
sriram <sriramsub7@gmail.com>
2021-04-12 11:50:14 +0530

    initial commit

---
+++ code/user.py
@@ -0,0 +1,10 @@
+
+class User:
+    def __init__(self):
+        self.__userid = ''
+        self.__password = ''
+        self.__loginStatus = False
+        self.__registerDate = ''
+
+    def register(self, id, pass):
+            self.
